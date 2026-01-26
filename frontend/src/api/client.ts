d0fd2c6bdb1d137eeae6c05fe04826789f225bc5
SofyaPalna <sosyenka@gmail.com>
2026-01-26 08:09:14 +0000

    do client ts

---
+++ frontend/src/api/client.ts
@@ -0,0 +1,43 @@
+class ApiErroR extends Error{
+    status: number
+    constructor(status:number, message:string){
+        super(message)
+        this.status = status
+        this.name = "ApiError"
+    }
+}
+
+class ApiClient{
+    private baseUrl: string
+
+    constructor(
+        baseUrl: string = `${import.meta.env.VITE_URL}/api`
+    ) {
+        this.baseUrl = baseUrl
+    }
+
+    private async request(
+        endpoint: string,
+        options?: RequestInit
+    ){
+        const url = `${this.baseUrl}${endpoint}`
+        const config : RequestInit = {...options,
+            headers:{
+                "Content-type":"apllication/json",
+                ...options?.headers
+            }
+        }
+        try {
+            const response = await fetch (url, config)
+
+            if (!response.ok) {
+                const errorData = await response.json().catch(()=> ({}))
+                throw new ApiError
+            }
+        } catch (error) {
+
+        }
+    }
+}
+
+export const apiClient = new ApiClient()
